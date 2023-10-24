53f8d5fe8770656a15692d97dd7211b6941db199
Yannick GALLY <ygally@gmail.com>
2023-10-25 00:37:04 +0200

    feat: add age calc

---
+++ src/Controller/DateAgeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Form\DateAgeFormType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DateAgeController extends AbstractController
+{
+    #[Route('/date_age', name: 'app_date_age')]
+    public function calculateAge(Request $request): Response
+    {
+        $form = $this->createForm(DateAgeFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $date = \DateTime::createFromFormat('Ymd', $data['date']);
+            $today = new \DateTime();
+            $interval = $date->diff($today);
+            $ageInDays = $interval->format('%R%a days');
+        } else {
+            $ageInDays = null;
+        }
+
+        return $this->render('date_age/index.html.twig', [
+            'form' => $form->createView(),
+            'ageInDays' => $ageInDays,
+        ]);
+    }
+}
---
+++ src/Form/DateAgeFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class DateAgeFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Regex([
+                        'pattern' => '/^\d{8}$/',
+                        'message' => 'Date should be in yyyyMMdd format',
+                    ]),
+                ],
+            ])
+            ->add('calculate', SubmitType::class, [
+                'label' => 'Calculate Age',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
